4da9e8199f89afdb1bdd97af0dc63b4882c87582
webuilddeveloper <webuild.developer@gmail.com>
2025-06-17 10:49:19 +0700

    add routing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from './pages/news/news.component';
+import { ProductAndServiceComponent } from './pages/product-and-service/product-and-service.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'product-and-service', component: ProductAndServiceComponent },
 ];
 
 @NgModule({
