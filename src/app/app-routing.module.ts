537f7e30704637c24ac860dca1f2ff610818628f
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-06-17 14:41:47 +0700

    update contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NewsComponent } from './pages/news/news.component';
 import { ProductAndServiceComponent } from './pages/product-and-service/product-and-service.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'news', component: NewsComponent },
   { path: 'product-and-service', component: ProductAndServiceComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,19 +1,30 @@
 import { Component, HostListener } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // เลื่อนไปบนสุดแบบ smooth
+      });
+  }
 
   scrollPosition: boolean = false;
-  @HostListener("window:scroll", [])
+  @HostListener('window:scroll', [])
   onWindowScroll() {
     let docElement = document.documentElement;
-    let scTop = (document.documentElement.clientHeight % 90);
-    let scrollTotal = docElement.scrollHeight - docElement.clientHeight
-    let aaa = docElement.scrollTop / (docElement.scrollHeight - docElement.offsetHeight);
+    let scTop = document.documentElement.clientHeight % 90;
+    let scrollTotal = docElement.scrollHeight - docElement.clientHeight;
+    let aaa =
+      docElement.scrollTop /
+      (docElement.scrollHeight - docElement.offsetHeight);
     // document.body.scrollHeight, document.documentElement.scrollHeight,
     // document.body.clientHeight, document.documentElement.clientHeight,
     // document.body.offsetHeight, document.documentElement.offsetHeight
@@ -22,11 +33,11 @@ export class HeaderComponent {
     // console.log('clientHeight ===>>>> ',document.documentElement.clientHeight);
     // console.log('offsetHeight ===>>>> ',document.documentElement.offsetHeight);
 
-
     if ((window.pageYOffset || document.documentElement.scrollTop) > 90) {
       this.scrollPosition = true;
-    }
-    else if ((window.pageYOffset || document.documentElement.scrollTop) < 90) {
+    } else if (
+      (window.pageYOffset || document.documentElement.scrollTop) < 90
+    ) {
       this.scrollPosition = false;
     }
   }
